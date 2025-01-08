ee71fc08afd207bbc1769b644c237a89b3669760
Heta-tatvasoft <heta.arora@tatvasoft.com>
2025-01-08 18:32:17 +0530

    updated primary theme color to blue

--- src/theme/index.js
+++ src/theme/index.js
@@ -4,7 +4,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#ff0000',
+      main: '#0c4a8e',
       contrastText: '#fff',
     },
     secondary: {
